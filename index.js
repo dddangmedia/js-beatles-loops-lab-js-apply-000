662e672e1561e227e1b0deeea459301516a40cb5
Duong Dang <dddangmedia@gmail.com>
2019-08-15 10:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(muscician, instrument) {
+  var list;
+  for (var i=0; i<muscician.length; i++) {
+    list.push(muscician[i] + " plays " + instruments[i]);
+  }
+  return list;
+}
